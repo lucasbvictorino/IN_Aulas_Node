ee932e90acd51bbc45dadfa56ac4c78a242d9221
Lucas Victorino <lucasvictorino@id.uff.br>
2026-02-17 15:50:33 -0300

    feat: adicionando validação do .env

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
 import { app } from './app.js'
+import { env } from './env/index.js'
 
-const host = '0.0.0.0'
-const port = 3333
+// const host = '0.0.0.0'
+// const port = 3333
 
 app
   .listen({
-    host,
-    port,
+    host: env.HOST,
+    port: env.PORT,
   })
   .then(() => {
-    const url = `http://localhost:${port}`
+    const url = `http://localhost:${env.PORT}`
     console.log(`HTTP Server RUnning at ${url}`)
   })
